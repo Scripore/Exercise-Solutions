bda6fc40d184b4e629acbb47a3bca9ae44d9965f
Scripore <Prongzhang@gmail.com>
2014-11-22 20:03:26 -0500

    pushing place_value.rb to repo

---
+++ place_value.rb
@@ -0,0 +1,34 @@
+# This program return the second digit in a four digital number in three different ways.
+
+
+puts "enter in a four digit number"
+
+x = gets.chomp.to_i
+
+def place_value (number)
+
+  first_num = number / 1000
+
+  first_num_zeros = 1000 * first_num
+  sec_num_z = number - first_num_zeros
+  result = sec_num_z / 100
+
+
+end
+
+puts place_value(x)
+
+
+puts "enter in a four digit number"
+x2 = gets.chomp
+
+puts x2[1]
+
+
+puts "enter in a four digit number"
+x3 = gets.chomp.to_i
+
+puts x3 % 1000 / 100
+
+
+
