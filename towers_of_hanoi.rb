880ce851bed60a857b2d4f5cd96298f49b7222c5
Scripore <Prongzhang@gmail.com>
2014-11-24 12:20:19 -0500

    updating

--- towers_of_hanoi.rb
+++ towers_of_hanoi.rb
@@ -1,4 +1,12 @@
 =begin
+
+
+
+
+INCOMPLETE - Still in Progress
+
+
+
 Towers of Hanoi game
 http://en.wikipedia.org/wiki/Tower_of_Hanoi
 "The Tower of Hanoi (also called the Tower of Brahma or Lucas' Tower,[1] and sometimes pluralized) is a mathematical game or puzzle. "
@@ -23,7 +31,9 @@ tower1 = [1,2,3]
 tower2 = []
 tower3 = []
 
-completed_tower = tower1
+completed_tower = [1,2,3]
+# when completed_tower = tower1 the while loop keep continuing even after tower3 is complete. when completed_tower = [1,2,3] the while loop stops. why?
+
 
 disc = 0
 
@@ -41,49 +51,58 @@ puts "enter 1, 2, or 3 to move to corresponding tower"
 
 tower_destination = gets.to_i
 
-if tower_selection == tower_destination
+  if tower_selection == tower_destination
 
-puts "you have picked the same tower."
+  puts "you have picked the same tower."
 
 
-else
+  else
 
-  if tower_selection == 1 && tower_destination == 2
-    tower2 << tower1.shift
+    if tower_selection == 1 && tower_destination == 2
+      if tower2[0].nil?
+        tower2 << tower1.shift
+      else
+        if tower1[0] < tower2[0] 
+          tower2 << tower1.shift
+        else
+          puts "try again! destination tower contains a disc smaller than currently selected disc."
+        end
+      end
+    end
+
+    if tower_selection == 1 && tower_destination == 3
+      tower3 << tower1.shift
+    end
+
+    if tower_selection == 2 && tower_destination == 1
+      tower1 << tower2.shift
+    end
+
+    if tower_selection == 2 && tower_destination == 3
+      tower3 << tower2.shift
+    end
+
+    if tower_selection == 3 && tower_destination == 1
+      tower1 << tower3.shift
+    end
+
+    if tower_selection == 3 && tower_destination == 2
+      tower2 << tower3.shift
+    end
+
+
+
+  p tower1
+  p tower2
+  p tower3
+
+
   end
 
-  if tower_selection == 1 && tower_destination == 3
-    tower3 << tower1.shift
+  if tower3 == completed_tower
+    is_tower3_complete = true
+    puts "Tower 3 is complete!! :O CONGRATULATIONS!! "
   end
-
-  if tower_selection == 2 && tower_destination == 1
-    tower1 << tower2.shift
-  end
-
-  if tower_selection == 2 && tower_destination == 3
-    tower3 << tower2.shift
-  end
-
-  if tower_selection == 3 && tower_destination == 1
-    tower1 << tower3.shift
-  end
-
-  if tower_selection == 3 && tower_destination == 2
-    tower2 << tower3.shift
-  end
-
-
-
-p tower1
-p tower2
-p tower3
-
-
-end
-
-if tower3 == completed_tower
- is_tower3_complete = true
-end
 
 end
 
