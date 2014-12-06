622cb7e4527c07f48b42947acca2bd15d3c81b16
Scripore <Prongzhang@gmail.com>
2014-12-06 16:08:37 -0500

    fixed clockangle.rb

--- clockangle.rb
+++ clockangle.rb
@@ -1,22 +1,8 @@
 
 def  clock_angles(_hour, _minute)
-
-  angle_one = _hour * 30
-
+  angle_one = _hour * 30 + (_minute/60.0 * 30)
   angle_two = _minute * 6
-
-  first_answer = angle_one - angle_two
-  second_answer = angle_two - angle_one
-
-
-  if first_answer > second_answer
-    return first_answer
-  else
-    return second_answer
-  end
-
-
-
+  first_answer = (angle_one - angle_two).abs
 end
 
-clock_angles( 2, 40)
+puts clock_angles( 3, 30)
