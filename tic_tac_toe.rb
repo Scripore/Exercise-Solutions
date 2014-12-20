9decd056622852de14fef59c4859a6f62876cfac
Scripore <Prongzhang@gmail.com>
2014-12-20 18:10:27 -0500

    updating tic_tac_toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,8 @@
 tic_tac_hash = {"1" => " ","2" => " ","3" => " ","4" => " ","5" => " ","6" => " ","7" => " ","8" => " ","9" => " "}
 tic_tac_array = []
 tally = 0
+winning_combinations = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']]
+
 
 def tic_tac_display(ttt_hash)
   puts "    |    |    "
@@ -28,6 +30,13 @@ while tally < 9 do
 
   puts "\nenter in a number selection:"
   user_pick = gets.chomp
+  while tic_tac_hash[user_pick] != ' '
+puts "USER, PLEASE PICK AN AVAILABLE SELECTION!!"
+p (tic_tac_hash.select {|x, y| y == ' '}).keys
+    user_pick = gets.chomp
+
+  end
+
 
   tic_tac_hash[user_pick] = "X"
   tally += 1
@@ -54,9 +63,24 @@ if tally < 9
   tic_tac_hash.each do |key, value|
     tic_tac_array << value
   end
-  p tic_tac_array
 
-  p tic_tac_array[1..3] == "O"
+  tic_tac_array[1..3] == "O"
+
+
+  winning_combinations.each do |combo|
+
+    if (tic_tac_hash[(combo[0])] == "X" || tic_tac_hash[(combo[0])] == "O") && (tic_tac_hash[(combo[0])] == tic_tac_hash[(combo[1])] && tic_tac_hash[(combo[0])] == tic_tac_hash[(combo[2])] )
+      case tic_tac_hash[(combo[0])]
+        when "X" then puts "YOU WINS"
+        else
+          puts "YOU LOSE!! BOOOOOO!!!!"
+      end
+      abort   # stops the program
+    end
+
+
+  end
+
 end
 
 
