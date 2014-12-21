439f57184a05aa9d94c8ed395849a1f9a5ab42ed
Scripore <Prongzhang@gmail.com>
2014-12-20 19:37:36 -0500

    updating tic_tac_toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,96 +1,68 @@
-#require 'pry'
-tic_tac_hash = {"1" => " ","2" => " ","3" => " ","4" => " ","5" => " ","6" => " ","7" => " ","8" => " ","9" => " "}
-tic_tac_array = []
-tally = 0
-winning_combinations = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']]
+#  By Ming Zhang
+#  Tic-Tac-Toe game submitted as part of the Flatiron School admissions process.
+#  Extensively commented the code.
 
 
-def tic_tac_display(ttt_hash)
-  puts "    |    |    "
-  puts " #{ttt_hash["1"]}  |#{ttt_hash["2"]}   |#{ttt_hash["3"]}   "
-  puts "    |    |    "
-  puts "--------------"
-  puts "    |    |    "
-  puts " #{ttt_hash["4"]}  |#{ttt_hash["5"]}   |#{ttt_hash["6"]}   "
-  puts "    |    |    "
-  puts "--------------"
-  puts "    |    |    "
-  puts " #{ttt_hash["7"]}  |#{ttt_hash["8"]}   |#{ttt_hash["9"]}   "
-  puts "    |    |    "
+tic_tac_hash = {"1" => " ","2" => " ","3" => " ","4" => " ","5" => " ","6" => " ","7" => " ","8" => " ","9" => " "}  # created a hash to hold the "X" and "O" marks.
+tally = 0 # tracking the moves
+winning_combinations = [['1','2','3'],['4','5','6'],['7','8','9'],['1','4','7'],['2','5','8'],['3','6','9'],['1','5','9'],['3','5','7']] # array of different winning combinations
+
+def tic_tac_display(ttt_hash)  # creating a method to draw the board, interpolating the hash values to string.
+  puts "     |     |     "
+  puts "  #{ttt_hash["1"]}  |  #{ttt_hash["2"]}  |  #{ttt_hash["3"]}  "
+  puts "     |     |     "
+  puts "-----------------"
+  puts "     |     |     "
+  puts "  #{ttt_hash["4"]}  |  #{ttt_hash["5"]}  |  #{ttt_hash["6"]}  "
+  puts "     |     |     "
+  puts "-----------------"
+  puts "     |     |     "
+  puts "  #{ttt_hash["7"]}  |  #{ttt_hash["8"]}  |  #{ttt_hash["9"]}  "
+  puts "     |     |     "
 end
 
+tic_tac_display(tic_tac_hash)   # drawing the board
+
+while tally < 9 do   # while there is less than 9 moves, execute the following
+  puts "\nenter in a number selection:"
+  user_pick = gets.chomp
+  while tic_tac_hash[user_pick] != ' '        # if the corresponding user selection does not result in a blank character then it is not a valid selection. raise the following error.
+    puts "USER, PLEASE PICK AN AVAILABLE SELECTION!!"
+    p (tic_tac_hash.select {|x, y| y == ' '}).keys  # displays what keys are available for selection
+    user_pick = gets.chomp
+  end
+
+  tic_tac_hash[user_pick] = "X"   # assigns "X" to hash with user selection as the hash key
+  tally += 1  # increment count by one
+
+  computer_selection = rand(1..9).to_s  # computer randomly selects a integer 1 to 9.
+
+  if tally < 9   # if the number of marks on the board is less than 9 then execute the following
+    while tic_tac_hash[computer_selection] != " "  # if computer selection does not correspond to a value containing a blank character then the program makes a new selection
+      computer_selection = rand(1..9).to_s
+    end
+    tic_tac_hash[computer_selection] = "O"  # assigning "O" to hash value that computer has selected
+    tally += 1  # increment count by 1
+
+    tic_tac_display(tic_tac_hash)  # draw updated board
+    puts "tally is: #{tally}"  # display current count
+
+
+
+    winning_combinations.each do |combo|  # iterates through the array. the if statement below checks to see if the elements in the subarrays all have the same value when embedded as a key in the tic_tac_hash.
+      if (tic_tac_hash[(combo[0])] == "X" || tic_tac_hash[(combo[0])] == "O") && (tic_tac_hash[(combo[0])] == tic_tac_hash[(combo[1])] && tic_tac_hash[(combo[0])] == tic_tac_hash[(combo[2])] )
+        case tic_tac_hash[(combo[0])]
+          when "X" then puts "YOU WIN!! GOOD JOB!!"
+          else
+            puts "YOU LOSE!! BOOOOOO!!!!"
+        end
+        abort   # stops the program if there is a winner.
+      end
+    end
+
+  end
+
+end
 
 tic_tac_display(tic_tac_hash)
 
-
-
-num_iterations = 0
-
-while tally < 9 do
-
-  puts "\nenter in a number selection:"
-  user_pick = gets.chomp
-  while tic_tac_hash[user_pick] != ' '
-puts "USER, PLEASE PICK AN AVAILABLE SELECTION!!"
-p (tic_tac_hash.select {|x, y| y == ' '}).keys
-    user_pick = gets.chomp
-
-  end
-
-
-  tic_tac_hash[user_pick] = "X"
-  tally += 1
-
-  computer_selection = rand(1..9).to_s
-
-
-
-if tally < 9
-  while tic_tac_hash[computer_selection] != " "
-    computer_selection = rand(1..9).to_s
-  end
-  tic_tac_hash[computer_selection] = "O"
-  tally += 1
-
-
-  p tic_tac_display(tic_tac_hash)
-  p "tally is: #{tally}"
-  num_iterations += 1
-
-#  binding.pry
-
-  tic_tac_array = []
-  tic_tac_hash.each do |key, value|
-    tic_tac_array << value
-  end
-
-  tic_tac_array[1..3] == "O"
-
-
-  winning_combinations.each do |combo|
-
-    if (tic_tac_hash[(combo[0])] == "X" || tic_tac_hash[(combo[0])] == "O") && (tic_tac_hash[(combo[0])] == tic_tac_hash[(combo[1])] && tic_tac_hash[(combo[0])] == tic_tac_hash[(combo[2])] )
-      case tic_tac_hash[(combo[0])]
-        when "X" then puts "YOU WINS"
-        else
-          puts "YOU LOSE!! BOOOOOO!!!!"
-      end
-      abort   # stops the program
-    end
-
-
-  end
-
-end
-
-
-end
-
-p tic_tac_display(tic_tac_hash)
-
-
-
-
-
-
-#if hash["1"] == hash["2"] && hash["3"] || hash["4"] == hash["5"] && hash["6"] || hash["7"] == hash["8"] && hash["9"] || hash["1"] == hash["4"] && hash["7"] || hash["2"] == hash["5"] && hash["8"] || hash["3"] == hash["6"] && hash["9"] || hash["1"] == hash["5"] && hash["9"] || hash["3"] == hash["5"] && hash["7"]
